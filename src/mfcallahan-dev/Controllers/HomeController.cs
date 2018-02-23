641a9696fd50f628422a5dc0dabeca7a53e719fc
Matthew Callahan <matthew.callahan@outlook.com>
2018-02-23 12:32:54 -0700

    initial commit

---
+++ src/mfcallahan-dev/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HomepageDev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/mfcallahan-dev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomepageDev.MvcApplication" Language="C#" %>
