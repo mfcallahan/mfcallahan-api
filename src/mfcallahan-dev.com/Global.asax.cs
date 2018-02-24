4ddf5640eaaead3b5d23bcaa63f29ed0248cb2bf
Matthew Callahan <matthew.callahan@outlook.com>
2018-02-24 14:04:17 -0700

    instaled bootstrap & jquery pkgs

--- src/mfcallahan-dev.com/Global.asax.cs
+++ src/mfcallahan-dev.com/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace HomepageDev
@@ -9,6 +10,7 @@ namespace HomepageDev
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
