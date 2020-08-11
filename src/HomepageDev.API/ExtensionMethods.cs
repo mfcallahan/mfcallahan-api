4d21bce3826929e6159daec4c99e1acada84bbaa
Matthew Callahan <matthew.callahan@outlook.com>
2020-08-11 10:51:21 -0700

    Add ExtensionMethods class

---
+++ src/HomepageDev.API/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace HomepageDev.API
+{
+    /// <summary>
+    /// Class to house custom extension methods created for this solution
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Add a key/value pair to an IDictionary&lt;TKey, TValue&gt; if the value is not null.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the IDictionary key</typeparam>
+        /// <typeparam name="TValue">The type of the IDictionary value</typeparam>
+        /// <param name="dictionary">The IDictionary to which the key/value pair will be added if the value is not null</param>
+        /// <param name="key">IDictionary key</param>
+        /// <param name="value">IDictionary value</param>
+        public static void AddIfNotNull<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (value != null)
+            {
+                dictionary.Add(key, value);
+            }
+        }
+    }
+}
---
+++ src/HomepageDev.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,46 @@
+﻿using HomepageDev.API;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HomepageDev.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class ExtensionMethodsTests
+    {
+        private Dictionary<string, string> Dict;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Dict = new Dictionary<string, string>() {
+                { "foo", "bar" }
+            };
+        }
+
+        [Test]
+        [TestCase("baz", "qux")]
+        public void AddIfNotNull_Should_Add_Key_Value_Pair_To_Dictionary_If_Value_Is_Not_Null(string key, string value)
+        {
+            var expectedDictLength = Dict.Count + 1;
+
+            Dict.AddIfNotNull(key, value);
+
+            Assert.That(Dict.Count == expectedDictLength);
+            Assert.That(Dict.ContainsKey(key) && Dict[key] == value);
+        }
+
+        [Test]
+        [TestCase("baz", null)]
+        public void AddIfNotNull_Should_Not_Add_Key_Value_Pair_To_Dictionary_If_Value_Is_Null(string key, string value)
+        {
+            var expectedDictLength = Dict.Count;
+
+            Dict.AddIfNotNull(key, value);
+
+            Assert.That(Dict.Count == expectedDictLength);
+            Assert.That(!Dict.ContainsKey(key));
+        }
+    }
+}
