9a3edea5bd1062149e69d38bafcf31063287375d
Matthew Callahan <matthew.callahan@outlook.com>
2020-08-13 15:57:40 -0700

    dev/prod environments configured

--- src/HomepageDev.API/Program.cs
+++ src/HomepageDev.API/Program.cs
@@ -12,8 +12,8 @@ namespace HomepageDev
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(
-            string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()
         );
     }
 }
